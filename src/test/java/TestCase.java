51f3cd902e525b841eb19fdbaf4438b1733c5df9
Matt Jasaitis <mattjasaitis@Matts-MBP.lan>
2022-10-06 13:23:17 -0700

    removed try/catch

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -14,31 +14,27 @@ public class TestCase {
     }
     @Test
     public void myTestCase() {
-        try {
-            driver.get("https://applitools.com/helloworld/");
-            WebElement numbers = driver.findElement(By.cssSelector("span.primary"));
-            WebElement button = driver.findElement(By.cssSelector("div.section:nth-child(3) > button:nth-child(1)"));
-            WebElement titleH = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(1)"));
-            WebElement titleD = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(11)"));
+        driver.get("https://applitools.com/helloworld/");
+        WebElement numbers = driver.findElement(By.cssSelector("span.primary"));
+        WebElement button = driver.findElement(By.cssSelector("div.section:nth-child(3) > button:nth-child(1)"));
+        WebElement titleH = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(1)"));
+        WebElement titleD = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(11)"));
 
-            Assertions.assertEquals(numbers.isDisplayed(), true);
-            Assertions.assertEquals(numbers.getText(), "123456");
-            Assertions.assertEquals(numbers.getCssValue("color"), "rgba(78, 90, 99, 1)");
+        Assertions.assertEquals(numbers.isDisplayed(), true);
+        Assertions.assertEquals(numbers.getText(), "123456");
+        Assertions.assertEquals(numbers.getCssValue("color"), "rgba(78, 90, 99, 1)");
 
-            Assertions.assertEquals(button.isDisplayed(), true);
-            Assertions.assertEquals(button.getText(), "Click me!");
-            Assertions.assertEquals(button.getCssValue("color"), "rgba(255, 255, 255, 1)");
+        Assertions.assertEquals(button.isDisplayed(), true);
+        Assertions.assertEquals(button.getText(), "Click me!");
+        Assertions.assertEquals(button.getCssValue("color"), "rgba(255, 255, 255, 1)");
 
-            Assertions.assertEquals(titleH.isDisplayed(), true);
-            Assertions.assertEquals(titleH.getText(), "H");
-            Assertions.assertEquals(titleH.getCssValue("color"), "rgba(255, 0, 0, 1)");
+        Assertions.assertEquals(titleH.isDisplayed(), true);
+        Assertions.assertEquals(titleH.getText(), "H");
+        Assertions.assertEquals(titleH.getCssValue("color"), "rgba(255, 0, 0, 1)");
 
-            Assertions.assertEquals(titleD.isDisplayed(), true);
-            Assertions.assertEquals(titleD.getText(), "D");
-            Assertions.assertEquals(titleD.getCssValue("color"), "rgba(70, 0, 255, 1)");
-        } catch(Exception e) {
-            Assertions.fail(e);
-        }
+        Assertions.assertEquals(titleD.isDisplayed(), true);
+        Assertions.assertEquals(titleD.getText(), "D");
+        Assertions.assertEquals(titleD.getCssValue("color"), "rgba(70, 0, 255, 1)");`
     }
 
     @AfterEach
