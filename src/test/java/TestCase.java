3113f283424450b82805b67a9eed125bbf6d4fd4
Matt Jasaitis <mattjasaitis@Matts-MacBook-Pro.local>
2022-11-17 15:32:49 -0800

    setup baselineBranchName

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -29,7 +29,9 @@ public class TestCase {
         suiteConfig.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
         suiteConfig.setBatch(myTestBatch);
         suiteConfig.setStitchMode(StitchMode.CSS);
-        suiteConfig.setBranchName("featureBranch1");
+        suiteConfig.setBranchName("featureBranch2");
+        suiteConfig.setParentBranchName("featureBranch1");
+        suiteConfig.setBaselineBranchName("default");
         suiteConfig.addBrowser(1000, 600, BrowserType.CHROME);
 //        suiteConfig.addBrowser(1600, 1200, BrowserType.FIREFOX);
 //        suiteConfig.addBrowser(1024, 768, BrowserType.SAFARI);
@@ -45,8 +47,8 @@ public class TestCase {
     }
 
     @Test
-    public void branchesTest_Two() {
-        driver.get("https://applitools.com/helloworld");
+    public void baselineBranchNameTest() {
+        driver.get("https://applitools.com/helloworld?diff2");
         eyes.check(Target.window());
     }
 
