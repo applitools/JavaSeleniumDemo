a665f0fbe52740d7e5e40c81718c39f9fcbfaedb
Matt Jasaitis <mattjasaitis@Matts-MBP.attlocal.net>
2022-11-22 14:21:56 -0800

    setting up github config

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestCase {
     private static WebDriver driver;
@@ -21,7 +22,9 @@ public class TestCase {
 
     @BeforeAll
     public static void beforeAll() {
-        driver = WebDriverManager.chromedriver().create();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = WebDriverManager.chromedriver().capabilities(chromeOptions).create();
         myTestBatch = new BatchInfo("Test Cases");
         testRunner = new VisualGridRunner(new RunnerOptions().testConcurrency(5));
 
@@ -29,9 +32,6 @@ public class TestCase {
         suiteConfig.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
         suiteConfig.setBatch(myTestBatch);
         suiteConfig.setStitchMode(StitchMode.CSS);
-        suiteConfig.setBranchName("featureBranch2");
-        suiteConfig.setParentBranchName("featureBranch1");
-        suiteConfig.setBaselineBranchName("default");
         suiteConfig.addBrowser(1000, 600, BrowserType.CHROME);
 //        suiteConfig.addBrowser(1600, 1200, BrowserType.FIREFOX);
 //        suiteConfig.addBrowser(1024, 768, BrowserType.SAFARI);
